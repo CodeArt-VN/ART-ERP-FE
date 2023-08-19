6d88d1d2f6680c3e456cbbecbb1c0fe1729cca5b
Dương Anh Phụng <phung.da@kdc.vn>
2023-08-19 23:25:33 +0700

    update components tabs

--- src/app/components/tabs-card/tabs-card.component.ts
+++ src/app/components/tabs-card/tabs-card.component.ts
@@ -1,25 +1,18 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-
+import { SchemaDetail } from '../../models/options-interface';
 
 @Component({
-  selector: 'app-tabs-card',
-  templateUrl: './tabs-card.component.html',
-  styleUrls: ['./tabs-card.component.scss'],
+	selector: 'app-tabs-card',
+	templateUrl: './tabs-card.component.html',
+	styleUrls: ['./tabs-card.component.scss'],
 })
 export class TabsCardComponent implements OnInit {
-  @Input() tabs = [];
-  @Input() type;
-  @Output() onChange = new EventEmitter();
+	@Input() tabs: SchemaDetail[];
+	@Input() type: string;
+	@Output() onChange = new EventEmitter<string>();
 
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-    
-  }
-  ngAfterViewInit() {
-    
-  }
-
-
+	ngOnInit() {}
+	ngAfterViewInit() {}
 }
