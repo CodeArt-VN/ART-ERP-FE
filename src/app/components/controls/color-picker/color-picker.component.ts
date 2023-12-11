7e50157bb6b3ddbff7edb6cdd5748aae02ad62db
Tan Phuc <bizyoh@gmail.com>
2023-12-11 14:59:32 +0700

    Init Color Picker & Icon Picker

---
+++ src/app/components/controls/color-picker/color-picker.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.scss'],
+})
+export class ColorPickerComponent  implements OnInit {
+	items = [
+		{Code:"primary", Name: "Primary"},
+		{Code:"secondary", Name: "Secondary"},
+		{Code:"tertiary", Name: "Tertiary"},
+
+		{Code:"success", Name: "Success"},
+		{Code:"warning", Name: "Warning"},
+		{Code:"danger", Name: "Danger"},
+
+		{Code:"red", Name: "Red"},
+		{Code:"pink", Name: "Pink"},
+		{Code:"purple", Name: "Purple"},
+
+    {Code:"blue", Name: "Blue"},
+		{Code:"bluegreen", Name: "Bluegreen"},
+
+		{Code:"dark", Name: "Dark"},
+		{Code:"medium", Name: "Medium"},
+		{Code:"light", Name: "Light"},
+
+	];
+	@Output() selected = new EventEmitter();
+	keyword = '';
+	constructor() {}
+
+	ngOnInit() {}
+	onSelected(event) {
+		this.selected.emit(event);
+	}
+}
