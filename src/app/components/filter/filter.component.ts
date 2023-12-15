0497f0cdd91c33444623cd460f6af013dc0f81cb
Tan Phuc <bizyoh@gmail.com>
2023-12-15 10:08:42 +0700

    Update Filter Component

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,8 +1,13 @@
 import { CdkDragMove, DragDropModule } from '@angular/cdk/drag-drop';
-import { CdkDragDrop,CdkDragEnter, moveItemInArray, transferArrayItem ,CdkDropList,CdkDragHandle} from '@angular/cdk/drag-drop';
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { FormBuilder, Validators, FormGroup, FormControl, FormArray } from '@angular/forms';
+import { CdkDragDrop, CdkDragEnter, moveItemInArray, transferArrayItem, CdkDropList, CdkDragHandle} from '@angular/cdk/drag-drop';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output,QueryList,ViewChild, ViewChildren} from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormControl, FormArray,} from '@angular/forms';
+import { BehaviorSubject, Subject, asapScheduler, catchError, combineLatest, concat, distinctUntilChanged, of, switchMap, tap,} from 'rxjs';
 import { FilterConfig, Schema } from 'src/app/models/options-interface';
+import { EnvService } from 'src/app/services/core/env.service';
+import { CRM_ContactProvider} from 'src/app/services/static/services.service';
+import { CommonService } from 'src/app/services/core/common.service';
+
 
 @Component({
 	selector: 'app-filter',
@@ -12,6 +17,7 @@ import { FilterConfig, Schema } from 'src/app/models/options-interface';
 export class FilterComponent implements OnInit {
 	form: FormGroup;
 	connectionList = [];
+	
 	transformOperators = [
 		{ code: 'TextGroup', name: 'Text', icon: '', disabled: true },
 		{ code: '=', name: 'is', icon: '' },
@@ -34,7 +40,7 @@ export class FilterComponent implements OnInit {
 
 		{ code: 'BooleanGroup', name: 'Boolean', icon: '', disabled: true },
 		{ code: '1', name: 'true', icon: '' },
-		{ code: '0', name: 'false', icon: '' }
+		{ code: '0', name: 'false', icon: '' },
 	];
 
 	logicalOperators = [
@@ -42,48 +48,27 @@ export class FilterComponent implements OnInit {
 		{ code: 'OR', name: 'OR', icon: '' },
 	];
 	//item sample
-	@Input() item: FilterConfig = 
-		{
-			Dimension: 'logical', Operator: 'AND',
-			Logicals: [
-				{ Dimension: 'Status', Operator: 'IN', Value: JSON.stringify(['New', 'Confirmed', 'Scheduled', 'Picking', 'Delivered', 'Done', 'Cancelled', 'InDelivery']) },
-				{ Dimension: 'Total', Operator: '=', Value: 'POSOrder' },
-			]
-		};
+	@Input() item;
+
+	@Input() schema: Schema;
+
+	constructor(
+		public formBuilder: FormBuilder,
+		public cdr: ChangeDetectorRef,
+	) {}
 	
-	@Input() schema: Schema = // schema sample;
-		{
-			Id: 1, Code: 'SaleOrder', Name: 'Sale orders', Type: 'Dataset', ModifiedDate: '2023-01-01',
-			Fields: [
-				{ IDSchema: null, Id: null, Code: 'logical', Name: 'Logical', Type: 'Logical', Icon: 'star', Aggregate: '', Sort: 1, Remark: '' },
-				//////////////////////////////
-				{ IDSchema: 1, Id: 1, Code: 'OrderDate', Name: 'Ngày lên đơn', Type: 'Text', Icon: 'star', Aggregate: '', Sort: 1, Remark: '' },
-				{ IDSchema: 1, Id: 1, Code: 'Status', Name: 'Status', Type: 'Text', Icon: 'star', Aggregate: '', Sort: 1, Remark: '' },
-				{ IDSchema: 1, Id: 1, Code: 'Count', Name: 'Count of documents', Type: 'Number', Icon: 'star', Aggregate: '', Sort: 1, Remark: '' },
-				{ IDSchema: 1, Id: 1, Code: 'Total', Name: 'Sum of total', Type: 'Number', Icon: 'star', Aggregate: '', Sort: 1, Remark: '' },
-				{ IDSchema: 1, Id: 1, Code: 'Discount', Name: 'Sum of discount', Type: 'Number', Icon: 'star', Aggregate: '', Sort: 1, Remark: '' },
-			]
-		};
-
-	constructor(public formBuilder: FormBuilder,public cdr: ChangeDetectorRef) {
-
-		
-
-	}
-
 	ngOnInit() {
-		if (!this.item) {
+		if (!this.item) {	
 			this.item = {
-				Dimension: 'logical', Operator: 'AND', Logicals: [
-					{ Dimension: null, Operator: null, Value: null },
-				]
+				Dimension: 'logical',
+				Operator: 'AND',
+				Logicals: [{ Dimension: null, Operator: null, Value: null }],
 			};
-
-			 this.form = this.buildForm(this.item);
-
-		}
-		else{
-			this.form =this.formBuilder.group({
+		
+			this.form = this.buildForm(this.item);
+		} else {
+			debugger
+			this.form = this.formBuilder.group({
 				Dimension: [this.item.Dimension, Validators.required],
 				Operator: [this.item.Operator, Validators.required],
 				Value: [
@@ -95,24 +80,33 @@ export class FilterComponent implements OnInit {
 				Logicals: this.formBuilder.array([]),
 				UniqueId: [{ value: this.generateUniqueId(), disabled: true }],
 			});
-			this.item.Logicals.forEach(x=>this.addForm(this.form,x))
+
+			this.item.Logicals.forEach((x) => this.addForm(this.form, x))
+			
 			this.updateConnectionList(this.form.controls.UniqueId.value);
 		}
 	}
 
 	ngAfterViewChecked() {
 		this.cdr.detectChanges();
-	 }
-	 enter(entered: CdkDragEnter<any>) {
-		console.log('entered');
-		console.log(entered);
 	}
+	
 	buildForm(c: FilterConfig) {
-		let notGroupList = ['MeasureBy', 'CompareBy', 'Interval', 'Transform', 'Schema', 'ReprotInfo'];
+		let notGroupList = [
+			'MeasureBy',
+			'CompareBy',
+			'Interval',
+			'Transform',
+			'Schema',
+			'ReprotInfo',
+		];
 		let keys = Object.keys(c);
 		let group: any = {};
-		keys.forEach(key => {
-			if (typeof c[key] === 'object' && notGroupList.findIndex(d => d == key) == -1) {
+		keys.forEach((key) => {
+			if (
+				typeof c[key] === 'object' &&
+				notGroupList.findIndex((d) => d == key) == -1
+			) {
 				if (Array.isArray(c[key])) {
 					for (let idx = 0; idx < c[key].length; idx++) {
 						const cObject = c[key][idx];
@@ -120,12 +114,10 @@ export class FilterComponent implements OnInit {
 						group[key] = new FormArray([]);
 						group[key].push(cGroup);
 					}
-				}
-				else {
+				} else {
 					group[key] = this.buildForm(c[key]);
 				}
-			}
-			else {
+			} else {
 				group[key] = new FormControl(c[key]);
 			}
 		});
@@ -133,47 +125,59 @@ export class FilterComponent implements OnInit {
 		return new FormGroup(group);
 	}
 
-
-	@Output() error = new EventEmitter();
-	onError(e) {
-		this.error.emit(e);
+	@Output() message = new EventEmitter();
+	getMessage(e) {
+		this.message.emit(e);
 	}
 
 	@Output() submit = new EventEmitter();
 	onFormSubmit(e) {
 		if (!this.form.valid) {
-			this.onError('erp.app.app-component.page-bage.check-red-above');
+		
+			this.getMessage({message:'erp.app.app-component.page-bage.check-red-above',logLevel:'warning'});
 			return;
 		}
-		this.submit.emit(this.form.getRawValue());
-	}
-
-	onChange(e){
-		console.log(e);
+		this.getMessage({message:'erp.app.app-component.page-bage.save-complete',logLevel:'success'});
+		this.submit.emit(this.form.value);
 	}
 
 	//Sau Khi Init => addForm => check Disable Dimension
 	//Sau khi addForm => check Disable Dimension
 	//Sau khi change Dimension =>check Disable Dimension
 	//Sau khi removeForm =>check Disable Dimension
-	addForm(fg: FormGroup,data:any=null) {
-		if(data==null){
+	addForm(fg: FormGroup, data: any = null) {
+		if (data == null) {
 			data = {};
 		}
 		let groups = <FormArray>fg.controls.Logicals;
-		
+		let selected = null;
+		if(data.Dimension=='ApprovedBy'){
+			selected= [data?.Value];
+		}
+
 		let group = this.formBuilder.group({
+	//		_staffDataSource :[{value:this.initSearchStaff(selected),disabled: true}],
 			UniqueId: [{ value: this.generateUniqueId(), disabled: true }],
-            
-			Dimension: [data.Dimension,[Validators.required]],
-            Operator: [data.Operator,[Validators.required]],
-            Value: [data.Value,[Validators.required]],
+			Dimension: [data.Dimension, [Validators.required]],
+			Operator: [data.Operator, [Validators.required]],
+			Value: [data.Value, [Validators.required]],
 			Logicals: this.formBuilder.array([]),
-        });
-		data.Logicals?.forEach(x=>this.addForm(group,x));
+		});
+		if(group.get('Dimension')?.value=='logical'){
+			group.get('Value').setValidators([]);
+			group.get('Value').updateValueAndValidity();
+		}
+
+		data.Logicals?.forEach((x) => this.addForm(group, x));
+
 		this.updateConnectionList(group.controls.UniqueId.value);
-        groups.push(group);
-		this.checkDiasableDimenson(fg);
+		if(data.Dimension==null){
+			groups.insert(0,group);
+		}
+		else{
+			groups.push(group);
+		}
+		this.checkDisableDimenson(fg);
 	}
 
 	changeDimension(fg: any) {
@@ -182,21 +186,20 @@ export class FilterComponent implements OnInit {
 			fg.get('Logicals').clear();
 			fg.get('Value').setValidators([Validators.required]);
 		} else {
-			fg.get('Operator').setValue("AND");
+			fg.get('Operator').setValue('AND');
 			fg.get('Value').setValidators([]);
 		}
 		fg.get('Value').updateValueAndValidity();
-		this.checkDiasableDimenson(fg);
+		this.checkDisableDimenson(fg);
 	}
 
 	//nếu Dimension=='logical' và  Logicals có child thì Disable Form Control Dimension
-	checkDiasableDimenson(fg: any){
+	checkDisableDimenson(fg: any) {
 		var dimension = fg.get('Dimension').value;
-		if(dimension =="logical"){
-			if(fg.get('Logicals').controls.length>0){
+		if (dimension == 'logical') {
+			if (fg.get('Logicals').controls.length > 0) {
 				fg.get('Dimension').disable();
-			}
-			else{
+			} else {
 				fg.get('Dimension').enable();
 			}
 		}
@@ -218,26 +221,36 @@ export class FilterComponent implements OnInit {
 				];
 			}
 		}
-		this.checkDiasableDimenson(parentForm)
+		this.checkDisableDimenson(parentForm);
 	}
-	
+
 	drop(event: CdkDragDrop<FormArray>) {
 		if (event.previousContainer === event.container) {
-		  moveItemInArray(event.container.data.controls, event.previousIndex, event.currentIndex);
+			moveItemInArray(
+				event.container.data.controls,
+				event.previousIndex,
+				event.currentIndex
+			);
 		} else {
-		  transferArrayItem(event.previousContainer.data.controls,
-							event.container.data.controls,
-							event.previousIndex,
-							event.currentIndex);
+			transferArrayItem(
+				event.previousContainer.data.controls,
+				event.container.data.controls,
+				event.previousIndex,
+				event.currentIndex
+			);
 		}
-	  }
-	
+	}
+
 	generateUniqueId(): string {
 		return Math.random().toString(36).slice(2, 5);
 	}
 
 	updateConnectionList(uniqueId) {
 		this.connectionList.push(uniqueId);
+	}
+
+	getSchemaDetailType(form){
+		return this.schema.Fields.find(x=>x.Code == form.get('Dimension')?.value).Type;
 	}
 
 }
