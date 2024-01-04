e903feb17b52ac7f70976673ce7b2f286d6b1965
phuc <bizyoh@gmail.com>
2024-01-04 19:02:02 +0700

    Build Form Report Config

--- src/app/components/visualizations/report-config/report-config.component.ts
+++ src/app/components/visualizations/report-config/report-config.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
+import { PopoverController } from '@ionic/angular';
 import { Subject, concat, of, distinctUntilChanged, tap, switchMap, catchError } from 'rxjs';
 import { BIReport, ReportDataConfig } from 'src/app/models/options-interface';
 import { ReportService } from 'src/app/services/report.service';
@@ -21,6 +22,7 @@ export class ReportConfigComponent implements OnInit {
 	_schemaDetailsList: any[] = [];
 	_timePeriodList: any[] = [];
 	_intervalDataSource:any[] = [];
+	_measureMethodDataSource:any[] = [];
 	_IDSchemaDataSource: any = {
 		searchProvider: this.schemaService,
 		loading: false,
@@ -46,7 +48,7 @@ export class ReportConfigComponent implements OnInit {
 	constructor(public rpt: ReportService,
 		public schemaService: SYS_SchemaProvider,
 		public formBuilder: FormBuilder,
-	) { }
+			) { }
 
 	ngOnInit() {
 		this._intervalDataSource =  [
@@ -58,64 +60,14 @@ export class ReportConfigComponent implements OnInit {
 			{ Code: 'Month', Name: 'Month' },
 			{ Code: 'Quater', Name: 'Quater' },
 			{ Code: 'Year', Name: 'Year' },
-		],
-
-		this.form = this.formBuilder.group({
-			TimeFrame: this.formBuilder.group({
-				Dimension: ['OrderDate'],
-				From: this.formBuilder.group({
-					Type: [''],
-					IsPastDate: [true],
-					Period: [''],
-					Amount: [0],
-				}),
-				To: this.formBuilder.group({
-					Type: [''],
-					IsPastDate: [true],
-					Period: [''],
-					Amount: [0],
-				}),
-			}),
-			CompareTo: this.formBuilder.group({
-				Type: ['Relative'],
-				IsPastDate: [true],
-				Period: ['Week'],
-				Amount: [1],
-			}),
-			Schema: this.formBuilder.group({
-				Id: [0],
-				Type: [''],
-				Code: [''],
-				Name: [''],
-			}),
-			Transform: this.formBuilder.group({
-				Filter: ['']
-			}),
-			Interval: this.formBuilder.group({
-				Property: ['OrderDate'],
-				Type: ['HourOfDay'],
-				Title: ['Order time'],
-			}),
-			CompareBy: this.formBuilder.array([
-				this.formBuilder.group({
-					Property: ['Status'],
-					Title: ['Status'],
-				}),
-			]),
-			MeasureBy: this.formBuilder.array([
-				this.formBuilder.group({
-					Property: ['Id'],
-					Method: ['count'],
-					Title: ['Count'],
-				}),
-				this.formBuilder.group({
-					Property: ['NumberOfGuests'],
-					Method: ['count'],
-					Title: ['NumberOfGuests'],
-				}),
-			]),
-		});
-		this._IDSchemaDataSource.initSearch()
+		];
+		this._measureMethodDataSource= [
+			{ Code: 'count', Name: 'Count {0}', icon: '' },
+			{ Code: 'sum', Name: 'Sum of {0}', icon: '' },
+			{ Code: 'max', Name: 'Max of {0}', icon: '' },
+			{ Code: 'min', Name: 'Min of {0}', icon: '' },
+			{ Code: 'average', Name: 'Average {0}', icon: '' },
+		];
 	}
 
 
@@ -125,16 +77,19 @@ export class ReportConfigComponent implements OnInit {
 		if (v) {
 			this._reportId = v;
 			this._reportConfig = this.rpt.getReportConfig(this._reportId);
-			if (this._reportConfig) {
-			//	this.form = this.buildForm(this._reportConfig.DataConfig);
-			}
-		//	this._schemaDetailsList = this.rpt.getSchemaDetail(this._reportConfig.DataConfig.Schema.Id);
+	
 			this._timePeriodList = JSON.parse(JSON.stringify(this.rpt.commonOptions.timeConfigPeriod));
 
 			this._timePeriodList.forEach(p => {
 				p.name = p.name.toLowerCase() + ' ago';
 			});
-
+		}
+		this.buildForm(	this._reportConfig.DataConfig);
+		let schema = this._reportConfig?.DataConfig?.Schema;
+		if(schema){
+			this._IDSchemaDataSource.selected.push(schema);
+			this._IDSchemaDataSource.initSearch();
+			this.changeSchema(schema);
 		}
 	}
 
@@ -149,10 +104,7 @@ export class ReportConfigComponent implements OnInit {
 			if(data){
 				this.selectedSchema = data;
 				this._schemaDetailsList = data.Fields;
-				this.config = null;
-				//this.form.get('Interval').setValue({'Property':this.})
 			}
-			
 		})
 	}
 
@@ -164,33 +116,129 @@ export class ReportConfigComponent implements OnInit {
 	onChange(event) {
 		console.log(this.form.getRawValue());
 	}
-
-
-	buildForm(c: ReportDataConfig) {
-		let notGroupList = ['MeasureBy', 'CompareBy', 'Interval', 'Transform', 'Schema', 'ReprotInfo'];
-
-		let keys = Object.keys(c);
-		let group: any = {};
-		keys.forEach(key => {
-
-			if (typeof c[key] === 'object' && notGroupList.findIndex(d => d == key) == -1) {
-				if (Array.isArray(c[key])) {
-					for (let idx = 0; idx < c[key].length; idx++) {
-						const cObject = c[key][idx];
-						let cGroup = this.buildForm(cObject);
-						group[key] = new FormArray([]);
-						group[key].push(cGroup);
-					}
-				}
-				else {
-					group[key] = this.buildForm(c[key]);
-				}
+	
+	PatchFormArrayValue(array){
+		let groups=  new FormArray([]);
+		if(array){
+			array.forEach(c=>{
+				let group :any ={};
+				let keys = Object.keys(c);
+				keys.forEach(key => {
+						group[key] = new FormControl(c[key]);
+					})
+				groups.push( new FormGroup(group))
+				});
 			}
-			else {
-				group[key] = new FormControl(c[key]);
+			return groups;
+	}
+	buildForm(c: ReportDataConfig) {//c: ReportDataConfig
+		if(!c){
+			c = {
+				TimeFrame: {
+					Dimension: 'OrderDate',
+					From: { Type: 'Relative', IsPastDate: true, Period: 'Day', Amount: 1 },
+					To: { Type: 'Relative', IsPastDate: true, Period: 'Day', Amount: 0 }
+				},
+				CompareTo: { Type: 'Relative', IsPastDate: true, Period: 'Week', Amount: 1 },
+				Schema: { Id: 1, Code: 'SALE_Order', Name: 'Sale orders', Type: 'DBTable' },
+				Transform: {
+					Filter: {
+						Dimension: 'logical', Operator: 'AND',
+						Logicals: [
+							{ Dimension: 'Status', Operator: 'IN', Value: JSON.stringify(['New', 'Confirmed', 'Scheduled', 'Picking', 'Delivered', 'Done', 'InDelivery']) }, //'Cancelled', 'Splitted', 'Merged',
+							{ Dimension: 'Type', Operator: '=', Value: 'POSOrder' },
+						]
+					},
+				},
+				Interval: { Property: 'OrderDate', Type: 'HourOfDay', Title: 'Order time' },
+				CompareBy: [
+					{ Property: 'Status', Title: 'Status' },
+				],
+				MeasureBy: [
+					{ Property: 'Id', Method: 'count', Title: 'Count' },
+					{ Property: 'NumberOfGuests', Method: 'count', Title: 'Guests' },
+					{ Property: 'CalcTotal', Method: 'sum', Title: 'Total' },
+				]
 			}
+		}
+		this.form = this.formBuilder.group({
+			TimeFrame: this.formBuilder.group({
+				Dimension: [c.TimeFrame?.Dimension],
+				From: this.formBuilder.group({
+					Type: [c.TimeFrame?.From?.Type],
+					IsPastDate: [c.TimeFrame?.From?.IsPastDate],
+					Period: [c.TimeFrame?.From?.Period],
+					Amount: [c.TimeFrame?.From?.Amount],
+				}),
+				To: this.formBuilder.group({
+					Type: [c.TimeFrame?.To?.Type],
+					IsPastDate: [c.TimeFrame?.To?.IsPastDate],
+					Period: [c.TimeFrame?.To?.Period],
+					Amount: [c.TimeFrame?.To?.Amount],
+				}),
+			}),
+			CompareTo: this.formBuilder.group({
+				Type: [c?.CompareTo?.Type],
+				IsPastDate: [c.CompareTo?.IsPastDate],
+				Period: [c.CompareTo?.Period],
+				Amount: [c.CompareTo?.Amount],
+			}),
+			Schema: this.formBuilder.group({
+				Id: [c.Schema?.Id],
+				Type: [c.Schema?.Type],
+				Code: [c.Schema?.Code],
+				Name: [c.Schema?.Name],
+			}),
+			Transform: this.formBuilder.group({
+				Filter: this.formBuilder.group({
+					
+				})
+			}),
+			Interval: this.formBuilder.group({
+				Property: [c.Interval?.Property],
+				Type: [c.Interval?.Type],
+				Title: [c.Interval?.Title],
+			}),
+			CompareBy: this.PatchFormArrayValue(c.CompareBy),
+			MeasureBy: this.PatchFormArrayValue(c.MeasureBy)
 		});
-		return new FormGroup(group);
+	
+		// let notGroupList = ['MeasureBy', 'CompareBy', 'Interval', 'Transform', 'Schema', 'ReprotInfo'];
+
+		// let keys = Object.keys(c);
+		// let group: any = {};
+		// keys.forEach(key => {
+
+		// 	if (typeof c[key] === 'object' && notGroupList.findIndex(d => d == key) == -1) {
+		// 		if (Array.isArray(c[key])) {
+		// 			for (let idx = 0; idx < c[key].length; idx++) {
+		// 				const cObject = c[key][idx];
+		// 				let cGroup = this.buildForm(cObject);
+		// 				group[key] = new FormArray([]);
+		// 				group[key].push(cGroup);
+		// 			}
+		// 		}
+		// 		else {
+		// 			group[key] = this.buildForm(c[key]);
+		// 		}
+		// 	}
+		// 	else {
+		// 		group[key] = new FormControl(c[key]);
+		// 	}
+		// });
+		// return new FormGroup(group);
+	}
+
+	@ViewChild('popoverPub') popoverPub;
+	isOpenPopover = false;
+	listControls:any;
+	formGroup:any;
+	tempForm:any;
+	presentPopover(event,fg, groupName) {
+		this.pickerGroupName = groupName;
+		this.formGroup = fg;
+		this.isOpenPopover = true;
+		this.popoverPub.event = event;
 	}
 
 
@@ -226,6 +274,26 @@ export class ReportConfigComponent implements OnInit {
 
 	}
 
+	segmentTimeframeView='s1';
+	segmentTimeframeChanged(e){
+		this.segmentTimeframeView=e.detail.value;
+	}
+	addMeasureByForm(e){
+		let group = this.formBuilder.group({
+			Property: [''],
+			Method: [''],
+			Title: [''],
+		});
+		let measureBy = this.form.get('MeasureBy') as FormArray;
+		measureBy.push(group);
+		this.presentPopover(e,group,'MeasureBy');
+	}
+	changeDateTime(e,key){
+		this.tempForm.get(key).setValue()
+		console.log(e.detail.value);
+		this.form.get('')
+	}
+
 	dismissDatePicker(apply: boolean = false) {
 		if (!this.isOpenDatePicker)
 			return;
@@ -238,6 +306,20 @@ export class ReportConfigComponent implements OnInit {
 			this.onRunReport();
 		}
 		this.isOpenDatePicker = false;
+	}
+
+	dismissPopoverPub(apply: boolean = false) {
+		if (!this.isOpenPopover)
+			return;
+
+		if (!apply) {
+			this.form.patchValue(this._reportConfig.DataConfig);
+		}
+		else {
+			this._reportConfig.DataConfig = this.form.getRawValue();
+			this.onRunReport();
+		}
+		this.isOpenPopover = false;
 	}
 
 	@Output() runReport = new EventEmitter();
