9e9d7a2eed2b379ee7af7cb988e27e27d78ca9b6
phuc <bizyoh@gmail.com>
2024-01-16 11:26:31 +0700

    delete filter => save lại

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -44,7 +44,7 @@ export class FilterComponent implements OnInit {
 	];
 	//item sample
 	_item: FilterConfig;
-	
+
 	@Input() smallWidth = false;
 
 	@Input() set item(i: FilterConfig) {
@@ -55,8 +55,8 @@ export class FilterComponent implements OnInit {
 				Logicals: [{ Dimension: null, Operator: null, Value: null }],
 			};
 		}
-		else{
-			if(i.Dimension!= 'logical'){
+		else {
+			if (i.Dimension != 'logical') {
 				i = {
 					Dimension: 'logical',
 					Operator: 'AND',
@@ -70,190 +70,191 @@ export class FilterComponent implements OnInit {
 
 
 
-@Input() schema: Schema;
+	@Input() schema: Schema;
 
-constructor(
-	public formBuilder: FormBuilder,
-	public cdr: ChangeDetectorRef
-) { }
+	constructor(
+		public formBuilder: FormBuilder,
+		public cdr: ChangeDetectorRef
+	) { }
 
-ngOnInit() {
-	this.buildForm();
-}
+	ngOnInit() {
+		this.buildForm();
+	}
 
-buildForm(){
-	this.form = this.formBuilder.group({
-		Dimension: [this._item.Dimension, Validators.required],
-		Operator: [this._item.Operator, Validators.required],
-		Value: [
-			this._item.Dimension !== 'logical' ? this._item.Value : null,
-			this._item.Dimension !== 'logical'
-				? [Validators.required]
-				: [],
-		],
-		Logicals: this.formBuilder.array([]),
-		UniqueId: [{ value: lib.generateUID(), disabled: true }],
-	});
-
-	this._item.Logicals.forEach((x) => this.addForm(this.form, x));
-	this.updateConnectionList(this.form.controls.UniqueId.value);
-}
-
-ngAfterViewChecked() {
-	this.cdr.detectChanges();
-}
-
-checkAndUpdateView() {
-	console.log('aaa')
-}
-@Output() message = new EventEmitter();
-getMessage(e) {
-	this.message.emit(e);
-}
-
-@Output() inputChange = new EventEmitter();
-onInputChange() {
-	if (!this.form.valid) {
-		this.getMessage({
-			message: 'erp.app.app-component.page-bage.check-red-above',
-			logLevel: 'warning',
+	buildForm() {
+		this.form = this.formBuilder.group({
+			Dimension: [this._item.Dimension, Validators.required],
+			Operator: [this._item.Operator, Validators.required],
+			Value: [
+				this._item.Dimension !== 'logical' ? this._item.Value : null,
+				this._item.Dimension !== 'logical'
+					? [Validators.required]
+					: [],
+			],
+			Logicals: this.formBuilder.array([]),
+			UniqueId: [{ value: lib.generateUID(), disabled: true }],
 		});
-		return;
+
+		this._item.Logicals.forEach((x) => this.addForm(this.form, x));
+		this.updateConnectionList(this.form.controls.UniqueId.value);
 	}
-	var i = this.form.getRawValue();
-	if(i.Dimension != 'logical'){
-		i = {
-			Dimension: 'logical',
-			Operator: 'AND',
-			Logicals: [i],
-		};
+
+	ngAfterViewChecked() {
+		this.cdr.detectChanges();
 	}
-	this.inputChange.emit(i);
-}
+
+	checkAndUpdateView() {
+		console.log('aaa')
+	}
+	@Output() message = new EventEmitter();
+	getMessage(e) {
+		this.message.emit(e);
+	}
+
+	@Output() inputChange = new EventEmitter();
+	onInputChange() {
+		if (!this.form.valid) {
+			this.getMessage({
+				message: 'erp.app.app-component.page-bage.check-red-above',
+				logLevel: 'warning',
+			});
+			return;
+		}
+		var i = this.form.getRawValue();
+		if (i.Dimension != 'logical') {
+			i = {
+				Dimension: 'logical',
+				Operator: 'AND',
+				Logicals: [i],
+			};
+		}
+		this.inputChange.emit(i);
+	}
 
 
-@Output() submit = new EventEmitter();
-onFormSubmit() {
-	if (!this.form.valid) {
-		this.getMessage({
-			message: 'erp.app.app-component.page-bage.check-red-above',
-			logLevel: 'warning',
+	@Output() submit = new EventEmitter();
+	onFormSubmit() {
+		if (!this.form.valid) {
+			this.getMessage({
+				message: 'erp.app.app-component.page-bage.check-red-above',
+				logLevel: 'warning',
+			});
+			return;
+		}
+		var i = this.form.getRawValue();
+		if (i.Dimension != 'logical') {
+			i = {
+				Dimension: 'logical',
+				Operator: 'AND',
+				Logicals: [i],
+			};
+		}
+		this.submit.emit(i);
+	}
+
+	//Sau Khi Init => addForm => check Disable Dimension
+	//Sau khi addForm => check Disable Dimension
+	//Sau khi change Dimension =>check Disable Dimension
+	//Sau khi removeForm =>check Disable Dimension
+	addForm(fg: FormGroup, data: any = null) {
+		if (data == null) {
+			data = {};
+		}
+		let groups = <FormArray>fg.controls.Logicals;
+
+		let group = this.formBuilder.group({
+			//		_staffDataSource :[{value:this.initSearchStaff(selected),disabled: true}],
+			UniqueId: [{ value: lib.generateUID(), disabled: true }],
+			Dimension: [data.Dimension, [Validators.required]],
+			Operator: [data.Operator, [Validators.required]],
+			Value: [data.Value, [Validators.required]],
+			Logicals: this.formBuilder.array([]),
 		});
-		return;
-	}
-	var i = this.form.getRawValue();
-	if(i.Dimension != 'logical'){
-		i = {
-			Dimension: 'logical',
-			Operator: 'AND',
-			Logicals: [i],
-		};
-	}
-	this.submit.emit(i);
-}
+		if (group.get('Dimension')?.value == 'logical') {
+			group.get('Value').setValidators([]);
+			group.get('Value').updateValueAndValidity();
+		}
 
-//Sau Khi Init => addForm => check Disable Dimension
-//Sau khi addForm => check Disable Dimension
-//Sau khi change Dimension =>check Disable Dimension
-//Sau khi removeForm =>check Disable Dimension
-addForm(fg: FormGroup, data: any = null) {
-	if (data == null) {
-		data = {};
-	}
-	let groups = <FormArray>fg.controls.Logicals;
+		data.Logicals?.forEach((x) => this.addForm(group, x));
 
-	let group = this.formBuilder.group({
-		//		_staffDataSource :[{value:this.initSearchStaff(selected),disabled: true}],
-		UniqueId: [{ value: lib.generateUID(), disabled: true }],
-		Dimension: [data.Dimension, [Validators.required]],
-		Operator: [data.Operator, [Validators.required]],
-		Value: [data.Value, [Validators.required]],
-		Logicals: this.formBuilder.array([]),
-	});
-	if (group.get('Dimension')?.value == 'logical') {
-		group.get('Value').setValidators([]);
-		group.get('Value').updateValueAndValidity();
-	}
-
-	data.Logicals?.forEach((x) => this.addForm(group, x));
-
-	this.updateConnectionList(group.controls.UniqueId.value);
-	if (data.Dimension == null) {
-		groups.insert(0, group);
-	} else {
-		groups.push(group);
-	}
-	this.checkDisableDimenson(fg);
-}
-
-changeDimension(fg: any) {
-	var dimension = fg.get('Dimension').value;
-	if (dimension != 'logical') {
-		fg.get('Logicals').clear();
-		fg.get('Value').setValidators([Validators.required]);
-	} else {
-		fg.get('Operator').setValue('AND');
-		fg.get('Value').setValidators([]);
-	}
-	fg.get('Value').updateValueAndValidity();
-	this.checkDisableDimenson(fg);
-	this.onInputChange();
-}
-
-//nếu Dimension=='logical' và  Logicals có child thì Disable Form Control Dimension
-checkDisableDimenson(fg: any) {
-	var dimension = fg.get('Dimension').value;
-	if (dimension == 'logical') {
-		if (fg.get('Logicals').controls.length > 0) {
-			fg.get('Dimension').disable();
+		this.updateConnectionList(group.controls.UniqueId.value);
+		if (data.Dimension == null) {
+			groups.insert(0, group);
 		} else {
-			fg.get('Dimension').enable();
+			groups.push(group);
+		}
+		this.checkDisableDimenson(fg);
+	}
+
+	changeDimension(fg: any) {
+		var dimension = fg.get('Dimension').value;
+		if (dimension != 'logical') {
+			fg.get('Logicals').clear();
+			fg.get('Value').setValidators([Validators.required]);
+		} else {
+			fg.get('Operator').setValue('AND');
+			fg.get('Value').setValidators([]);
+		}
+		fg.get('Value').updateValueAndValidity();
+		this.checkDisableDimenson(fg);
+		this.onInputChange();
+	}
+
+	//nếu Dimension=='logical' và  Logicals có child thì Disable Form Control Dimension
+	checkDisableDimenson(fg: any) {
+		var dimension = fg.get('Dimension').value;
+		if (dimension == 'logical') {
+			if (fg.get('Logicals').controls.length > 0) {
+				fg.get('Dimension').disable();
+			} else {
+				fg.get('Dimension').enable();
+			}
 		}
 	}
-}
 
-removeForm(fg: FormGroup, parentForm: FormGroup): void {
-	if(parentForm && parentForm.controls?.Logicals instanceof FormArray) {
-	const index = parentForm.controls.Logicals?.controls.indexOf(fg);
-	if (index !== -1) {
-		parentForm.controls.Logicals.removeAt(index);
-		[
-			...this.connectionList.filter(
-				(x) =>
-					x !=
-					this.connectionList.indexOf(
-						fg.controls.UniqueId.value
-					)
-			),
-		];
+	removeForm(fg: FormGroup, parentForm: FormGroup): void {
+		if (parentForm && parentForm.controls?.Logicals instanceof FormArray) {
+			const index = parentForm.controls.Logicals?.controls.indexOf(fg);
+			if (index !== -1) {
+				parentForm.controls.Logicals.removeAt(index);
+				[
+					...this.connectionList.filter(
+						(x) =>
+							x !=
+							this.connectionList.indexOf(
+								fg.controls.UniqueId.value
+							)
+					),
+				];
+			}
+		}
+		this.checkDisableDimenson(parentForm);
+		this.onInputChange();
 	}
-}
-this.checkDisableDimenson(parentForm);
+
+	drop(event: CdkDragDrop<FormArray>) {
+		if (event.previousContainer === event.container) {
+			moveItemInArray(
+				event.container.data.controls,
+				event.previousIndex,
+				event.currentIndex
+			);
+		} else {
+			transferArrayItem(
+				event.previousContainer.data.controls,
+				event.container.data.controls,
+				event.previousIndex,
+				event.currentIndex
+			);
+		}
 	}
 
-drop(event: CdkDragDrop<FormArray>) {
-	if (event.previousContainer === event.container) {
-		moveItemInArray(
-			event.container.data.controls,
-			event.previousIndex,
-			event.currentIndex
-		);
-	} else {
-		transferArrayItem(
-			event.previousContainer.data.controls,
-			event.container.data.controls,
-			event.previousIndex,
-			event.currentIndex
-		);
+	updateConnectionList(uniqueId) {
+		this.connectionList.push(uniqueId);
 	}
-}
 
-updateConnectionList(uniqueId) {
-	this.connectionList.push(uniqueId);
-}
-
-getSchemaDetailType(form) {
-	let field = this.schema.Fields.find((x) => x.Code == form.get('Dimension')?.value);
-	return field?.DataType;
-}
+	getSchemaDetailType(form) {
+		let field = this.schema.Fields.find((x) => x.Code == form.get('Dimension')?.value);
+		return field?.DataType;
+	}
 }
