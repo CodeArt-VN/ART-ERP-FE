67124f203a89a75f7dae54fc9cec43e208e824cd
hungvq <hungvq@live.com>
2024-03-16 18:02:19 +0700

    style + update submodules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,7 +81,7 @@ let allRoutes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(allRoutes, { preloadingStrategy: PreloadAllModules, useHash: true })
+    RouterModule.forRoot(allRoutes, { preloadingStrategy: PreloadAllModules, useHash: false })
   ],
   exports: [RouterModule]
 })
