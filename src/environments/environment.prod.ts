fda51647a4eb13c6d9f008a43edd323b969b92d1
hungvq <hungvq@live.com>
2024-04-06 11:59:29 +0700

    IN 0.19.68

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export let environment = {
   production: true,
-  appVersion: '0.19.64',
+  appVersion: '0.19.68',
   //appDomain: window.location.origin + '/',
   //appDomain: 'https://demo.inholdings.vn/',
-  appDomain: 'https://app.inholdings.vn/',
+  appDomain: 'https://api.inholdings.vn/',
   //appDomain: 'https://beta.inholdings.vn/',
   appServers: [
-    { Code: 'https://app.inholdings.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
+    { Code: 'https://api.inholdings.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
     { Code: 'https://beta.inholdings.vn/', Name: 'Beta server', Color: 'warning', Icon: 'cube-outline' },
     { Code: 'https://demo.inholdings.vn/', Name: 'Demo server', Color: 'danger', Icon: 'airplane' },
   ],
@@ -15,7 +15,7 @@ export let environment = {
   apiVersion: 'api/v1/',
   showScrollbar: true, // (navigator.appVersion.indexOf("Win") > -1)
   signalRServiceDomain: 'https://signalrservice.appcenter.vn/',
-  appStoreURL: 'http://itunes.apple.com/',
+  appStoreURL: 'https://itunes.apple.com/',
   playStoreURL: 'https://play.google.com/',
   loginEmail: '@inholdings.vn',
 };
