199bb258c83098df0eb5c9434dd0ec313913c228
Hùng Vũ <hungvq@live.com>
2024-06-12 15:59:00 +0700

    release 0.19.76

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export let environment = {
   production: true,
-  appVersion: '0.19.70',
+  appVersion: '0.19.76',
   //appDomain: window.location.origin + '/',
   //appDomain: 'https://demo.inholdings.vn/',
   appDomain: 'https://api.inholdings.vn/',
