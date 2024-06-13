70a49671e64396d202e9ce7e779767d78a45e033
thienvo <111thienvo@gmail.com>
2024-06-13 15:44:52 +0700

    update integration + page title

--- src/app/pages/SYS/integration-provider/integration-provider.page.ts
+++ src/app/pages/SYS/integration-provider/integration-provider.page.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  NavController,
-  ModalController,
-  AlertController,
-  LoadingController,
-  PopoverController,
-} from '@ionic/angular';
+import { NavController, ModalController, AlertController, LoadingController, PopoverController } from '@ionic/angular';
 import { EnvService } from 'src/app/services/core/env.service';
 import { PageBase } from 'src/app/page-base';
 import {
