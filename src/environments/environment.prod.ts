f348a967353d5076f4970ed5be0589f5b61449cc
Hùng Vũ <hungvq@live.com>
2024-07-02 16:36:01 +0700

    release 0.19.75

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: '0.19.69',
+  appVersion: '0.19.75',
   appDomain: 'https://artlogistics.vn/',
   appServers: [
     { Code: 'https://artlogistics.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
