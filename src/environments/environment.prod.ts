fa73ec1d538617447a053e58dcc1f1f8b436cd75
Hùng Vũ <hungvq@live.com>
2025-03-03 09:44:54 +0700

    release 0.20.29

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 export const environment = {
-  production: true,
-  appVersion: '0.20.22',
-  appDomain: 'https://artlogistics.vn/',
-  appServers: [
-    { Code: 'https://artlogistics.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
-    { Code: 'https://beta.artlogistics.vn/', Name: 'Beta server', Color: 'warning', Icon: 'cube-outline' },
-    { Code: 'https://demo.artlogistics.vn/', Name: 'Demo server', Color: 'danger', Icon: 'airplane' },
-  ],
-  staffAvatarsServer: "https://artlogistics.vn/Uploads/HRM/Staffs/Avatars/",
-  posImagesServer: "https://artlogistics.vn/",
-  apiVersion: "api/v1/",
-  showScrollbar: true, // (navigator.appVersion.indexOf("Win") > -1)
-  signalRServiceDomain: 'https://signalrservice.appcenter.vn/',
-  appStoreURL: 'http://itunes.apple.com/lb/app/art-dms/id1540404648?mt=8',
-  playStoreURL: 'https://play.google.com/store/apps/details?id=vn.codeart.art.dms2&hl=vn',
-  loginEmail: '@artlogistics.vn'
+	production: true,
+	appVersion: '0.20.29',
+	appDomain: 'https://artlogistics.vn/',
+	appServers: [
+		{ Code: 'https://artlogistics.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
+		{ Code: 'https://beta.artlogistics.vn/', Name: 'Beta server', Color: 'warning', Icon: 'cube-outline' },
+		{ Code: 'https://demo.artlogistics.vn/', Name: 'Demo server', Color: 'danger', Icon: 'airplane' },
+	],
+	staffAvatarsServer: 'https://artlogistics.vn/Uploads/HRM/Staffs/Avatars/',
+	posImagesServer: 'https://artlogistics.vn/',
+	apiVersion: 'api/v1/',
+	showScrollbar: true, // (navigator.appVersion.indexOf("Win") > -1)
+	signalRServiceDomain: 'https://signalrservice.appcenter.vn/',
+	appStoreURL: 'http://itunes.apple.com/lb/app/art-dms/id1540404648?mt=8',
+	playStoreURL: 'https://play.google.com/store/apps/details?id=vn.codeart.art.dms2&hl=vn',
+	loginEmail: '@artlogistics.vn',
 };
