447bd75a5b5b6e11d63fa259e36e4ce8595004d5
loipham <loipham21101998@gmail.com>
2025-03-11 17:15:06 +0700

    update toolbarCommandRules

--- src/app/services/static/toolbar-command-rules.ts
+++ src/app/services/static/toolbar-command-rules.ts
@@ -1,9 +1,182 @@
 export var toolbarCommandRules = {
 	getRules: function (serviceName) {
+		console.log('Matrix button : ', this.convertArrayToObjects(this[serviceName]));
 		return this[serviceName] || [];
 	},
 
+	convertArrayToObjects(matrix) {
+		if (!matrix) return [];
+
+		const headers = matrix[0];
+		const result = [];
+
+		for (let i = 1; i < matrix.length; i++) {
+			const row = matrix[i];
+			const status = row[0];
+			const showBtns = [];
+
+			for (let j = 1; j < row.length; j++) {
+				if (row[j] === 'x') {
+					showBtns.push(`Show${headers[j]}`);
+				}
+			}
+
+			result.push({ Status: status, ShowBtns: showBtns });
+		}
+
+		return result;
+	},
+
 	PURCHASE_Request: [
+		['Btns', 'ChangeBranch', 'Merge', 'Split', 'Submit', 'Disapprove', 'Approve', 'Cancel', 'Delete', 'Archive', 'CopyToPurchaseQuotation', 'CopyToPO '],
+		['Draft', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', null, ' '],
+		['Splitted', null, null, null, null, null, null, null, null, 'x', null, ' '],
+		['Merged', null, null, null, null, null, null, null, null, 'x', null, ' '],
+		['Canceled', null, null, null, null, null, null, null, 'x', 'x', null, ' '],
+		['Submitted', null, null, null, null, 'x', 'x', null, null, null, null, ' '],
+		['Unapproved', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', null, ' '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, 'x', 'x', 'x '],
+		['Open', null, null, null, null, null, null, null, null, 'x', null, ' '],
+		['Closed', null, null, null, null, null, null, null, null, 'x', null, null],
+	],
+
+	PURCHASE_Quotation: [
+		[
+			'Btns',
+			'ChangeBranch',
+			'Merge',
+			'Split',
+			'Submit',
+			'Disapprove',
+			'Approve',
+			'Cancel',
+			'Delete',
+			'Archive',
+			'Confirm',
+			'SendQuotationRequest',
+			'CopyToPurchaseRequest',
+			'CopyToPO',
+			'CopyToPriceListVersion ',
+		],
+		['Draft', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', null, 'x', null, null, ' '],
+		['Splitted', null, null, null, null, null, null, null, null, 'x', null, null, null, null, ' '],
+		['Merged', null, null, null, null, null, null, null, null, 'x', null, null, null, null, ' '],
+		['Canceled', null, null, null, null, null, null, null, 'x', 'x', null, null, null, null, ' '],
+		['Open', null, null, null, 'x', null, 'x', 'x', null, 'x', 'x', null, null, null, ' '],
+		['Confirmed', null, null, null, 'x', null, 'x', 'x', null, 'x', null, 'x', null, null, ' '],
+		['Submitted', null, null, null, null, 'x', 'x', null, null, null, null, null, null, null, ' '],
+		['Unapproved', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', null, 'x', null, null, ' '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, 'x', null, null, 'x', 'x', 'x '],
+		['Closed', null, null, null, null, null, null, null, null, 'x', null, null, null, null, 'x'],
+	],
+
+	PURCHASE_Order: [
+		[
+			'Btns',
+			'ChangeBranch',
+			'Merge',
+			'Split',
+			'Submit',
+			'Disapprove',
+			'Approve',
+			'Cancel',
+			'Delete',
+			'Archive',
+			'SubmitOrders',
+			'CopyToReceipt',
+			'CopyToAPInvoice',
+			'RequestOutgoingPayment ',
+		],
+		['Draft', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', null, null, null, ' '],
+		['Splitted', null, null, null, null, null, null, null, null, 'x', null, null, null, ' '],
+		['Merged', null, null, null, null, null, null, null, null, 'x', null, null, null, ' '],
+		['Canceled', null, null, null, null, null, null, null, 'x', 'x', null, null, null, ' '],
+		['Unapproved', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', null, null, null, ' '],
+		['Submitted', null, null, null, null, 'x', 'x', null, null, null, null, null, null, ' '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, 'x', 'x', null, null, ' '],
+		['Open', null, null, null, null, null, null, null, null, 'x', null, null, null, ' '],
+		['Closed', null, null, null, null, null, null, null, null, 'x', null, null, null, ' '],
+		['Ordered', null, null, null, null, null, null, null, null, 'x', null, 'x', 'x', 'x '],
+		['Confirmed', null, null, null, null, null, null, null, null, null, null, 'x', 'x', 'x '],
+		['Shipping', null, null, null, null, null, null, null, null, null, null, 'x', 'x', 'x '],
+		['PartiallyReceived', null, null, null, null, null, null, null, null, null, null, 'x', 'x', 'x '],
+		['Received', null, null, null, null, null, null, null, null, null, null, 'x', 'x', 'x'],
+	],
+
+	WMS_Receipt: [
+		['Btns', 'ChangeBranch', 'Cancel', 'Delete', 'Archive', 'Delivery', 'Receive', 'CopyToAPInvoice '],
+		['New', 'x', 'x', 'x', 'x', null, null, ' '],
+		['Confirmed', null, 'x', 'x', 'x', 'x', null, ' '],
+		['Scheduled', null, 'x', null, 'x', 'x', null, ' '],
+		['Delivering', null, null, null, 'x', null, 'x', ' '],
+		['Palletized', null, null, null, 'x', null, 'x', ' '],
+		['Unloading', null, null, null, 'x', null, null, ' '],
+		['Received', null, null, null, 'x', null, null, 'x '],
+		['Canceled', null, null, null, 'x', null, null, null],
+	],
+
+	AC_APInvoice: [
+		['Btns', 'Delete', 'Archive', 'RequestOutgoingPayment '],
+		['Draft', 'x', 'x', ' '],
+		['Open', null, 'x', 'x '],
+		['Canceled', null, 'x', ' '],
+		['Closed', null, 'x', null],
+	],
+
+	BANK_OutgoingPayment: [
+		['Btns', 'ChangeBranch', 'Merge', 'Split', 'Submit', 'Disapprove', 'Approve', 'Cancel', 'Delete', 'Archive', 'MarkAsPaid', 'Pay '],
+		['NotSubmittedYet', 'x', 'x', 'x', null, null, 'x', 'x', 'x', 'x', null, ' '],
+		['Canceled', null, null, null, null, null, null, null, 'x', null, null, ' '],
+		['Unapproved', 'x', 'x', 'x', 'x', null, null, 'x', 'x', 'x', null, ' '],
+		['Submitted', 'x', 'x', 'x', null, 'x', 'x', null, 'x', 'x', null, ' '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, null, 'x', 'x '],
+		['WaitForPayment', null, null, null, null, null, null, null, null, null, 'x', 'x '],
+		['PartiallyPaid', null, null, null, null, null, null, null, null, null, 'x', 'x '],
+		['Paid', null, null, null, null, null, null, null, null, null, null, null],
+	],
+
+	SALE_Quotation: [
+		['Btns', 'ChangeBranch', 'Merge', 'Split', 'Submit', 'Disapprove', 'Approve', 'Cancel', 'Delete', 'Archive', 'Confirm '],
+		['Open', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', ' '],
+		['Splitted', null, null, null, null, null, null, null, null, 'x', ' '],
+		['Merged', null, null, null, null, null, null, null, null, 'x', ' '],
+		['Confirmed', null, null, null, null, null, null, null, null, null, ' '],
+		['Submitted', null, null, null, null, 'x', 'x', null, null, null, ' '],
+		['Unapproved', null, null, null, 'x', null, 'x', 'x', 'x', 'x', ' '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, null, 'x '],
+		['Canceled', null, null, null, null, null, null, null, null, 'x', ' '],
+		['Closed', null, null, null, null, null, null, null, 'x', 'x', null],
+	],
+
+	SALE_Order: [
+		['Btns', 'ChangeBranch', 'Merge', 'Split', 'Submit', 'Disapprove', 'Approve', 'Cancel', 'Delete', 'Archive', 'CopyToARInvoice '],
+		['Draft', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', ' '],
+		['New', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', ' '],
+		['Splitted', null, null, null, null, null, null, null, null, 'x', ' '],
+		['Merged', null, null, null, null, null, null, null, null, 'x', ' '],
+		['Canceled', null, null, null, null, null, null, null, 'x', 'x', ' '],
+		['Submitted', null, null, null, null, 'x', 'x', null, null, null, ' '],
+		['Unapproved', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x', ' '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, 'x', 'x '],
+		['Scheduled', null, null, null, null, null, null, null, null, 'x', 'x '],
+		['InCarrier', null, null, null, null, null, null, null, null, 'x', 'x '],
+		['Picking', null, null, null, null, null, null, null, null, 'x', 'x '],
+		['InDelivery', null, null, null, null, null, null, null, null, 'x', 'x '],
+		['Delivered', null, null, null, null, null, null, null, null, 'x', 'x '],
+		['Redelivery', null, null, null, null, null, null, null, null, 'x', ' '],
+		['Debt', null, null, null, null, null, null, null, null, 'x', 'x '],
+		['Done', null, null, null, null, null, null, null, null, 'x', 'x'],
+	],
+
+	CRM_Contact: [
+		['Btns', 'ChangeBranch', 'Merge', 'Split', 'Submit', 'Disapprove', 'Approve', 'Cancel', 'Delete', 'Archive '],
+		['New', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x '],
+		['Submitted', null, null, null, null, 'x', 'x', null, null, ' '],
+		['Unapproved', 'x', 'x', 'x', 'x', null, 'x', 'x', 'x', 'x '],
+		['Approved', null, null, null, null, 'x', null, 'x', null, 'x'],
+	],
+
+	_PURCHASE_Request: [
 		{ Status: 'Draft', ShowBtns: ['ShowChangeBranch', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Mới
 		{ Status: 'Unapproved', ShowBtns: ['ShowChangeBranch', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Không duyệt
 		{ Status: 'Submitted', ShowBtns: ['ShowApprove', 'ShowDisapprove'] },
@@ -16,7 +189,7 @@ export var toolbarCommandRules = {
 		{ Status: 'Merged', ShowBtns: ['ShowArchive'] },
 		{ Status: 'Canceled', ShowBtns: ['ShowArchive'] },
 	],
-	PURCHASE_Quotation: [
+	_PURCHASE_Quotation: [
 		{ Status: 'Draft', ShowBtns: ['ShowSplit', 'ShowMerge', 'ShowSendQuotationRequest'] },
 		{ Status: 'Open', ShowBtns: ['ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] },
 		{ Status: 'Unapproved', ShowBtns: ['ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive', 'ShowSendQuotationRequest'] },
@@ -26,7 +199,7 @@ export var toolbarCommandRules = {
 		{ Status: 'Canceled', ShowBtns: ['ShowDelete', 'ShowArchive'] },
 		{ Status: 'Closed', ShowBtns: ['ShowCopyToPriceListVersion'] },
 	],
-	PURCHASE_Order: [
+	_PURCHASE_Order: [
 		{ Status: 'Draft', ShowBtns: ['ShowChangeBranch', 'ShowMerge', 'ShowSplit', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] },
 		{ Status: 'Unapproved', ShowBtns: ['ShowChangeBranch', 'ShowMerge', 'ShowSplit', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] },
 		{ Status: 'Submitted', ShowBtns: ['ShowApprove', 'ShowDisapprove'] },
@@ -45,7 +218,7 @@ export var toolbarCommandRules = {
 		{ Status: 'Canceled', ShowBtns: ['ShowDelete', 'ShowArchive'] }, // Đã hủy
 	],
 
-	SALE_Quotation: [
+	_SALE_Quotation: [
 		{ Status: 'Open', ShowBtns: ['ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] },
 		{ Status: 'Unapproved', ShowBtns: ['ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] },
 		{ Status: 'Submitted', ShowBtns: ['ShowApprove', 'ShowDisapprove'] },
@@ -54,7 +227,7 @@ export var toolbarCommandRules = {
 		{ Status: 'Canceled', ShowBtns: ['ShowDelete', 'ShowArchive'] },
 	],
 
-	SALE_Order: [
+	_SALE_Order: [
 		{ Status: 'New', ShowBtns: ['ShowChangeBranch', 'ShowMerge', 'ShowSplit', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Mới
 		{ Status: 'Unapproved', ShowBtns: ['ShowChangeBranch', 'ShowMerge', 'ShowSplit', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Không duyệt
 		{ Status: 'Submitted', ShowBtns: ['ShowApprove', 'ShowDisapprove'] }, // Chờ duyệt
@@ -72,14 +245,14 @@ export var toolbarCommandRules = {
 		{ Status: 'Canceled', ShowBtns: ['ShowDelete', 'ShowArchive'] }, // Đã hủy
 	],
 
-	CRM_Contact: [
+	_CRM_Contact: [
 		{ Status: 'New', ShowBtns: ['ShowChangeBranch', 'ShowMerge', 'ShowSplit', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Mới
 		{ Status: 'Unapproved', ShowBtns: ['ShowChangeBranch', 'ShowMerge', 'ShowSplit', 'ShowSubmit', 'ShowApprove', 'ShowCancel', 'ShowDelete', 'ShowArchive'] }, // Không duyệt
 		{ Status: 'Submitted', ShowBtns: ['ShowApprove', 'ShowDisapprove'] }, // Chờ duyệt
 		{ Status: 'Approved', ShowBtns: ['ShowDisapprove', 'ShowCopyToARInvoice', 'ShowCancel', 'ShowArchive'] }, // Đã duyệt
 	],
 
-	WMS_Receipt: [
+	_WMS_Receipt: [
 		{ Status: 'New', ShowBtns: ['ShowChangeBranch', 'ShowCancel', 'ShowDelete', 'ShowArchive'] },
 		{ Status: 'Confirmed', ShowBtns: ['ShowCancel', 'ShowDelete', 'ShowArchive', 'ShowDelivery'] },
 		{ Status: 'Scheduled', ShowBtns: ['ShowCancel', 'ShowArchive', 'ShowDelivery'] },
