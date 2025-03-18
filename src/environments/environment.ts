b952b34219d1a783d973ea7df1629ce1b967bee6
Hùng Vũ <hungvq@live.com>
2025-03-19 00:11:38 +0700

    update data-table css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export let environment = {
 	//appDomain: 'https://demo.inholdings.vn/',
 	//appDomain: 'https://beta.inholdings.vn/',
 	//appDomain: 'https://artlogistics.vn/',
-	//appDomain: "http://10.211.55.3:54009/",
-	appDomain: 'http://art.appcenter.vn:54009/',
+	appDomain: "http://10.211.55.3:54009/",
+	//appDomain: 'http://art.appcenter.vn:54009/',
 	appServers: [
 		{ Code: 'https://artlogistics.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
 		{ Code: 'https://beta.artlogistics.vn/', Name: 'Beta server', Color: 'warning', Icon: 'cube-outline' },
