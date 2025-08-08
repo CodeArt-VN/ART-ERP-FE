0ea6c050a02493f457d788025a71dc51e0b6ab06
Hùng Vũ <hungvq@live.com>
2025-08-08 09:34:54 +0700

    release 0.20.93

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,14 @@
 export let environment = {
 	production: true,
-	appVersion: '0.20.41',
-	//appDomain: window.location.origin + '/',
-	//appDomain: 'https://demo.inholdings.vn/',
-	appDomain: 'https://api.inholdings.vn/',
-	//appDomain: 'https://beta.inholdings.vn/',
+	appVersion: '0.20.93',
+	appLocation: '/ERP/', // VirtualDirectory
+	versionLocation: 'V{{REPLACE_VERSION}}/',
 	appServers: [
 		{ Code: 'https://api.inholdings.vn/', Name: 'Main server', Color: 'success', Icon: 'rocket' },
 		{ Code: 'https://demo1.appcenter.vn/', Name: 'Demo server 1', Color: 'danger', Icon: 'airplane' },
 		{ Code: 'https://demo2.appcenter.vn/', Name: 'Demo server 2', Color: 'danger', Icon: 'airplane' },
 	],
+	appDomain: 'https://api.inholdings.vn/',
 	staffAvatarsServer: 'https://api.inholdings.vn/Uploads/HRM/Staffs/Avatars/',
 	posImagesServer: 'https://api.inholdings.vn/',
 	apiVersion: 'api/v1/',
