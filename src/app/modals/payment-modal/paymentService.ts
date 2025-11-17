fa278d1158de1b3ae79a6c2dd3afcc5e0483dcda
bizyoh <bizyoh@gmail.com>
2025-11-17 14:50:13 +0700

    payment modal

--- src/app/modals/payment-modal/paymentService.ts
+++ src/app/modals/payment-modal/paymentService.ts
@@ -12,9 +12,9 @@ export class PaymentService {
     constructor(private commonService: CommonService, private env: EnvService) {
     }
 
-    public async checkResultRequest(IDPayment): Promise<any> {
-        return this.commonService.connect('POST', 'BANK/IncomingPayment/CheckPaymentResult', { IDPayment: IDPayment }).toPromise()
-    }
+    // public async checkResultRequest(IDPayment): Promise<any> {
+    //     return this.commonService.connect('GET', 'BANK/IncomingPayment/CheckPaymentResult', { IDPayment: IDPayment }).toPromise()
+    // }
     public async postEDCCSale(payment) {
         return new Promise((resolve, reject) => {
             this.commonService.connect('POST', "BANK/IncomingPayment/PostEDCCSale", payment).toPromise().then(rs => {
