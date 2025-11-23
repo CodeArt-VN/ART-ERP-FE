4dac9b5ee15152e8852bbe3f7b62f2bce12b7c0d
Hùng Vũ <hungvq@live.com>
2025-11-23 14:56:50 +0700

    fix print

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	appVersion: '0.21.21',
+	appVersion: '0.21.22',
 	appLocation: '/ERP/', // VirtualDirectory
 	versionLocation: 'V{{REPLACE_VERSION}}/',
 	appServers: [
