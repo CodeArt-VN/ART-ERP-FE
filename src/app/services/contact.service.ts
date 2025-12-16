0178f10e99aa0c9fd0fea7ef065576e99776a010
loipham <loipham21101998@gmail.com>
2025-12-16 08:53:24 +0700

    update file

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { CommonService } from './core/common.service';
 import { EnvService } from './core/env.service';
 import { SYS_ConfigService } from './custom/system-config.service';
-import { CRM_ContactProvider } from './static/services.service';
-
+import { CRM_ContactProvider, SYS_ConfigOptionProvider } from './static/services.service';
 
 @Injectable({
 	providedIn: 'root',
 })
-
 export class CRM_ContactService extends CRM_ContactProvider {
 	constructor(
 		public sysConfigService: SYS_ConfigService,
+		public sysConfigOptionProvider: SYS_ConfigOptionProvider,
+
 		public env: EnvService,
 		public commonService: CommonService
 	) {
@@ -23,7 +23,7 @@ export class CRM_ContactService extends CRM_ContactProvider {
 	 * @param IDBranch Branch ID to load config for (null for current selected branch)
 	 * @returns Promise resolving to ConfigList when config is loaded successfully
 	 */
-	getConfig(IDBranch = null,keys) {
+	getConfig(IDBranch = null, keys) {
 		return new Promise((resolve, reject) => {
 			this.sysConfigService
 				.getConfig(IDBranch ?? this.env.selectedBranch, keys)
@@ -36,5 +36,15 @@ export class CRM_ContactService extends CRM_ContactProvider {
 		});
 	}
 
-
+	getConfigOptionCode(sysConfigOptionCode: string[]) {
+		return new Promise((resolve, reject) => {
+			// let sysConfigOptionCode = ['CRMContactLotable', 'CRMContactUDF'];
+			this.sysConfigOptionProvider
+				.read({ Code_in: sysConfigOptionCode, AllChildren: true })
+				.then((configOption: any) => {
+					resolve(configOption.data.filter((d) => !configOption.data.some((s) => s.IDParent == d.Id)).map((d) => d.Code));
+				})
+				.catch((err) => reject(err));
+		});
+	}
 }
