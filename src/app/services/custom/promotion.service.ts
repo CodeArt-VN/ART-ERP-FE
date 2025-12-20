a2ea3614823366792c8740e9eb6917ee0434e035
tanphuc <bizyoh@gmail.com>
2025-12-20 12:30:53 +0700

    update voucher service

--- src/app/services/custom/promotion.service.ts
+++ src/app/services/custom/promotion.service.ts
@@ -9,8 +9,8 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class PromotionService {
 	promotionList: any[] = [];
-	private appliedPromotionsSource = new BehaviorSubject<any[]>([]);
-	appliedPromotions$ = this.appliedPromotionsSource.asObservable();
+	private voucherBySO$ = new BehaviorSubject<Record<number, any[]>>({});
+	voucherBySOObs$ = this.voucherBySO$.asObservable();
 	constructor(
 		public commonService: CommonService,
 		public programProvider: PR_ProgramProvider,
@@ -24,18 +24,64 @@ export class PromotionService {
 				this.promotionList = value.data;
 			});
 	}
-	updateAppliedPromotions(list: any[]) {
-		this.appliedPromotionsSource.next(list);
+
+	getPromotionProgram(IDSO) {
+		// trả ra FE trước cho smooth UI rồi di lấy voucher BE
+		const map = { ...this.voucherBySO$.value };
+		this.voucherBySO$.next(map);
+		return  this.programProvider.commonService
+			.connect('GET', 'PR/Program/AppliedProgramInSaleOrder', {
+				IDSO: IDSO,
+			})
+			.toPromise()
+			.then((data: any) => {
+		// update voucher Mới nhất
+				map[IDSO] = data || [];
+				this.voucherBySO$.next(map);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
-	deleteVoucher(saleOrder, listCode) {
+
+	applyVoucher(so: any, code: string) {
+		return new Promise((resolve, reject) => {
+			this.commonService
+				.connect('POST', 'PR/Program/UseVoucher/', {
+					VoucherCodeList: [code],
+					SaleOrder: so,
+					IsCheckOnly: false,
+				})
+				.toPromise()
+				.then((savedItem: any) => {
+					const map = { ...this.voucherBySO$.value };
+					map[so.Id] = [...(map[so.Id] || []), code];
+					this.voucherBySO$.next(map);
+					this.env.showMessage('Saving completed!', 'success');
+					resolve(savedItem);
+				})
+				.catch((err) => {
+					this.env.showErrorMessage(err);
+					resolve(null);
+				});
+		});
+	}
+
+	deleteVoucher(saleOrder: any, voucherCodes: string[]) {
 		return new Promise((resolve, reject) => {
 			this.commonService
 				.connect('POST', 'PR/Program/UnUseVoucher/', {
 					SaleOrder: saleOrder,
-					VoucherCodeList: listCode,
+					VoucherCodeList: voucherCodes,
 				})
 				.toPromise()
-				.then((savedItem: any) => {
+				.then(() => {
+					const map = { ...this.voucherBySO$.value };
+
+					map[saleOrder.Id] = (map[saleOrder.Id] || []).filter((v) => !voucherCodes.includes(v.VoucherCode));
+
+					this.voucherBySO$.next(map);
+
 					this.env.showMessage('Saving completed!', 'success');
 					resolve(true);
 				})
@@ -46,17 +92,9 @@ export class PromotionService {
 		});
 	}
 
-	getPromotionProgram(IDSO) {
-		this.programProvider.commonService
-			.connect('GET', 'PR/Program/AppliedProgramInSaleOrder', {
-				IDSO: IDSO,
-			})
-			.toPromise()
-			.then((data: any) => {
-				this.appliedPromotionsSource.next(data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+	clearMemory(IDSO: number) {
+		const map = { ...this.voucherBySO$.value };
+		delete map[IDSO];
+		this.voucherBySO$.next(map);
 	}
 }
