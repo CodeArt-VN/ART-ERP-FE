3f55d7a877e746709be1f15712dd5c21e88c9c0b
Hùng Vũ <hungvq@live.com>
2026-01-15 08:46:57 +0700

    V0.21.34

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	appVersion: '0.21.33',
+	appVersion: '0.21.34',
 	appLocation: '/ERP/', // VirtualDirectory
 	versionLocation: 'V{{REPLACE_VERSION}}/',
 	appServers: [
