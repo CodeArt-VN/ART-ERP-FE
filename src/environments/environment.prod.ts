b39dc99bc62c9e405be8a8d0514f00692a4d790d
Hùng Vũ <hungvq@live.com>
2026-09-17 08:11:38 +0700

    0.22.37

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	appVersion: '0.22.36',
+	appVersion: '0.22.37',
 	appLocation: '/', // VirtualDirectory
 	versionLocation: 'V{{REPLACE_VERSION}}/', // 'V{{REPLACE_VERSION}}/',
 	appServers: [
